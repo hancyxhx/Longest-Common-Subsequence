f8fc1b96fd32b0b600ce30430e44a95798f51527
hancy <hancyxhx@gmail.com>
2014-03-20 22:24:34 +0800

    update sample program

--- sample_main.c
+++ sample_main.c
@@ -3,12 +3,19 @@
 
 
 int main(int argc, char *argv[]){
-    char *lcs_str;
-    printf("7:%d\n", LCS("zuotian", "zQuQoQtQiQaQnQ", &lcs_str));
-    printf("%s\n", lcs_str);
+    /* To get sample_s2, I insert 'Q' between every character of sample_s1.
+       So the length of the longest common subsequence of the two strings
+       is 9, which is the length of sample_s1 */
+    char *sample_s1 = "yesterday";
+    char *sample_s2 = "yQeQsQtQeQrQdQaQy"; 
+    printf("first sample string: %s\n", sample_s1);
+    printf("second sample string: %s\n", sample_s2);
     
-    printf("3:%d\n", LCS("abc", "dabbc", &lcs_str));
-    printf("%s\n", lcs_str);
+    char *lcs_str;    
+    int len = LCS(sample_s1, sample_s2, &lcs_str);
+    printf("the length of the longest common subsequence is: %d\n", len);
+    printf("the longest common subsequence is: %s\n", lcs_str);
+    
     return 0;
 }
 
